8b6b9717b0257551cd67556a1c60066d2e933943
Fr0zenBoy <th129513@gmail.com>
2019-09-09 23:16:55 -0300

    calculate currency

--- src/dinizeitor/price/quotation.clj
+++ src/dinizeitor/price/quotation.clj
@@ -1,9 +1,32 @@
 (ns dinizeitor.price.quotation
   (:require [clj-http.client :as client]
-            [cheshire.core :refer :all]))
+            [cheshire.core :refer :all]
+            [clojurewerkz.money.currencies :as cu]
+            [clojurewerkz.money.amounts :refer [amount-of]]
+            [clojurewerkz.money.format :as money]))
 
+(import java.util.Locale)
+
+
+(money/format (amount-of cu/BRL 1.02 ) (Locale. "pt" "BR"))
 
 (defn get-quoutes []
   (-> (client/get "https://api.hgbrasil.com/finance" {:accept :json})
       :body
       (parse-string true)))
+
+
+(def dollar-status (get-in (get-quoutes) [:results :currencies :USD]))
+
+(defn round2
+  [precision d]
+  (let [factor (Math/pow 10 precision)]
+    (/ (Math/round (* d factor)) factor)))
+
+;; ((partial round2 2) 4.885555555555) => 4.89
+
+(defn converter-coin [{:keys [buy] :as dollar-status} value coin]
+  (cond
+    (= (clojure.string/upper-case coin) "BRL") (/ buy value)
+    (= (clojure.string/upper-case coin) "USD") (* value buy)))
+
